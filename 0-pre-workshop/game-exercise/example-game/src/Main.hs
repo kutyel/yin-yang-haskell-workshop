61b6d23735c84b21e7b354678abcacc3bf191947
kutyel <flaviocorpa@gmail.com>
2019-10-12 18:25:19 +0200

    Run hfmt

--- 0-pre-workshop/game-exercise/example-game/src/Main.hs
+++ 0-pre-workshop/game-exercise/example-game/src/Main.hs
@@ -4,16 +4,23 @@ import           Graphics.Gloss                       hiding (Circle)
 import           Graphics.Gloss.Interface.IO.Interact hiding (Circle)
 
 main :: IO ()
-main = play (InWindow "yin-yang" (300, 300) (100, 100))
-            black
-            20
-            initial
-            view
-            input
-            step
+main =
+  play
+    (InWindow "yin-yang" (300, 300) (100, 100))
+    black
+    20
+    initial
+    view
+    input
+    step
 
-data World = Go Shape Int | Pause Int
-data Shape = Circle | Square
+data World
+  = Go Shape Int
+  | Pause Int
+
+data Shape
+  = Circle
+  | Square
 
 initial :: World
 initial = Go Circle 0
@@ -37,4 +44,5 @@ makeC x = makeColorI x 150 150 255
 view :: World -> Picture
 view (Go Square n) = color (makeC n) (rectangleSolid 100 100)
 view (Go Circle n) = color (makeC n) (thickCircle 50 50)
-view (Pause _) = translate (-120) 0 $ scale 0.2 0.2 $ color white $ text "Press r to resume"
+view (Pause _) =
+  translate (-120) 0 $ scale 0.2 0.2 $ color white $ text "Press r to resume"
