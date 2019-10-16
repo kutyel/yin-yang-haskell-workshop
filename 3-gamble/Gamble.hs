8e2f4e15deb079f89104b2ffb1be2f4903be4b72
kutyel <flaviocorpa@gmail.com>
2019-10-16 15:31:42 +0200

    Start fixing exercise 3! 🎲

--- 3-gamble/Gamble.hs
+++ 3-gamble/Gamble.hs
@@ -1,48 +1,51 @@
-{-# language DeriveFunctor #-}
-{-# language TypeSynonymInstances #-}
-{-# language FlexibleInstances #-}
-module Gamble where
+{-# LANGUAGE DeriveFunctor        #-}
+{-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE TypeSynonymInstances #-}
 
-import Control.Monad.State
-import System.Random
+module Main where
+
+import           Control.Monad.State
+import           System.Random
 
 main :: IO ()
-main
-  = do r <- game 10
-       if r then putStrLn "win"
-            else putStrLn "lose"
+main = do
+  r <- game 10
+  if r
+    then putStrLn "win"
+    else putStrLn "lose"
 
 -- A very stupid game:
 --  throw two n-sided dices,
 --  if the first is larger than the second, you win
 --  otherwise, you lose
 game :: Integer -> IO Bool
-game n
-  = do x <- randomElement [1 .. n]
-       y <- randomElement [1 .. n]
-       return (x > y)
+game n = do
+  x <- randomElement [1 .. n]
+  y <- randomElement [1 .. n]
+  return (x > y)
 
 -- Get a random element from a list
 randomElement :: [a] -> IO a
-randomElement []
-  = error "Empty lists are not supported"
-randomElement xs
-  = do -- generate a random index
-       ix <- randomRIO (0, length xs - 1)
-       return (xs !! ix)
+randomElement [] = error "Empty lists are not supported"
+randomElement xs = do
+  ix <- randomRIO (0, length xs - 1) -- generate a random index
+  return (xs !! ix)
 
-class Monad m => GambleMonad m where
+class Monad m =>
+      GambleMonad m
+  where
   roll :: [a] -> m a
 
 -- EXERCISE 1. Rewrite `game` using `GambleMonad`
-
 game' :: GambleMonad m => Integer -> m Bool
-game' n = undefined
+game' n = do
+  x <- roll [1 .. n]
+  y <- roll [1 .. n]
+  return (x > y)
 
 -- EXERCISE 2. Write the following instances of `GambleMonad`
-
 instance GambleMonad IO where
-  roll = undefined
+  roll = randomElement
 
 -- TreeOfPossibilities records every possibility
 data TreeOfPossibilities a
@@ -53,10 +56,14 @@ data TreeOfPossibilities a
 -- TreeOfPossibilities is a monad!
 instance Applicative TreeOfPossibilities where
   pure = Result
-  f <*> x = do { f' <- f ; x' <- x ; return (f' x') }
+  f <*> x = do
+    f' <- f
+    x' <- x
+    return (f' x')
+
 instance Monad TreeOfPossibilities where
   return = Result
-  Result x         >>= f = f x
+  Result x >>= f = f x
   ChooseBetween xs >>= f = ChooseBetween [x >>= f | x <- xs]
 
 instance GambleMonad TreeOfPossibilities where
@@ -67,9 +74,7 @@ type Scripted = State [Int]
 
 instance GambleMonad Scripted where
   roll = undefined
-
 -- EXERCISE 3. Write the `UserChoiceMonad` class
-
 {-
 factorGame :: (GambleMonad m, UserChoiceMonad m)
            => Integer -> Integer -> m Bool
